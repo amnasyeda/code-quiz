0d9b2958dc7c40956623e2ecadb73affd022abdc
amnasyeda <amna.syeda98@gmail.com>
2021-05-11 18:10:07 -0400

    script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Variables 
+var timeElement = document.querySelector("#time");
+var wrapperElement = document.querySelector(".wrapper");
+var startBtn = document.querySelector("#start");
+var questionsElement = document.querySelector("#questions");
+var choicesElement = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialInput = document.querySelector("#initial-input");
+var feedbackElement = document.querySelector("#feedback");
